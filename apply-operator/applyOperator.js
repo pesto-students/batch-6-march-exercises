2bc74b844c59b3abfdfd527f2e3c4c2ef4300b12
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-03-28 00:05:36 +0530

    Complete boowho test

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,30 @@
 
 function applyOperator(...args) {
-  return args;
+  if(args[0] === undefined){
+    throw new Error('operator is not defined');
+  }
+  var operator = args[0];
+  var result = args[1] !== undefined ? args[1] : 0;
+  for (var i = 2; i < args.length; i++){
+    switch (operator){
+      case '+':
+        result = result + args[1];
+        break;
+      case '-':
+        result = result - args[1];
+        break;
+      case '*':
+        result = result * args[1];
+        break;
+      case '/':
+        result = result / args[1];
+        break;
+      case '%':
+        result = result % args[1];
+        break;      
+    }
+  }
+  return result;
 }
 
 export {
