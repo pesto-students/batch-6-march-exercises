1cf7d0793e27e907e172473b251854c0f6f4c411
Abdul Khuddus <khuddus1@gmail.com>
2019-03-28 01:59:13 +0530

    Is triangle completed

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,10 @@
 
 function isTriangle(...args) {
-  return args;
+  var firstSide = args[0];
+  var secondSide = args[1];
+  var thirdSide = args[0];
+  var sumOfSides = firstSide + secondSide + thirdSide;
+  return ( firstSide < ( sumOfSides / 2 ) ) && ( secondSide < ( sumOfSides / 2 ) ) && ( thirdSide < ( sumOfSides / 2) ) ;
 }
 
 export {
