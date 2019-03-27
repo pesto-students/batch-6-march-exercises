93dbe5e89643e5a756a3fa6ac7f62e2e2a1ade58
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-03-28 04:03:31 +0530

    fix variable names

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,12 +1,12 @@
-function factorial(n) {
-  if (n === 0 || n === 1) {
+function factorial(number) {
+  if (number === 0 || number === 1) {
     return 1;
   }
   let result = 1;
-  let i = 1;
-  while (i <= n) {
-    result *= i;
-    i += 1;
+  let initialNumber = 1;
+  while (initialNumber <= number) {
+    result *= initialNumber;
+    initialNumber += 1;
   }
   return result;
 }
