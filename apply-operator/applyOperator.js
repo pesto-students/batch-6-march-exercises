75627fc3f2f621b9bbd9cf51a68e28470eec31e9
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-03-28 04:04:03 +0530

    ApplyOperator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,50 +1,17 @@
 
 function applyOperator(...args) {
-  if(typeof args[0] === 'undefined'){
+  var operator = args[0];
+  if(operator === undefined){
     throw new Error('operator is not defined');
   }
-  var operator = args[0];
-  var result;
-  for (var i = 1; i < args.length; i++){
-    switch (operator){
-      case '+':
-        if(typeof result === 'undefined'){
-          result = 0;
-        }
-        result = result + args[i];
-        break;
-      case '-':
-        if(typeof result === 'undefined'){
-          result = 0;
-        }
-        result = result - args[i];
-        break;
-      case '*':
-        if(typeof result === 'undefined'){
-          result = 1;
-        }
-        result = result * args[i];
-        break;
-      case '/':
-        if(typeof result === 'undefined'){
-          result = 1;
-        }
-        result = result / args[i];
-        break;
-      case '%':
-        if(typeof result === 'undefined'){
-          result = args[i];
-        }
-        else{
-          result = result % args[i];  
-        }
-        break;      
-    }
-  }
-  if(typeof result === 'undefined'){
-    return 0;
-  }
-  return result;
+  // args.shift();
+  // var operands = args;
+
+  // if(operands.length === 0) {
+  //   return 0;
+  // }
+    
+  return 0;
 }
 
 export {
