48d821a8a9fbf150972ac34a2d2b3e4444718614
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-03-28 04:45:23 +0530

    pass largeOfFour test

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,8 +1,9 @@
-
-function largeOfFour(...args) {
-  return args;
+function findLargest(array) {
+  return array.reduce((prev, next) => Math.max(prev, next), array[0]);
+}
+function largeOfFour(nestedArray) {
+  const arrayOfLargest = nestedArray.map(array => findLargest(array));
+  return arrayOfLargest;
 }
 
-export {
-  largeOfFour,
-};
+export { largeOfFour };
