d1bb59a5c551ca8be7b0053bfb791692110c2d97
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-03-28 04:13:40 +0530

    pass sumAll test

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,8 +1,19 @@
-
-function sumAll(...args) {
-  return args;
+function sumAll([firstNumber, secondNumber]) {
+  let min;
+  let max;
+  let sum = 0;
+  if (firstNumber > secondNumber) {
+    min = secondNumber;
+    max = firstNumber;
+  } else {
+    min = firstNumber;
+    max = secondNumber;
+  }
+  while (min <= max) {
+    sum += min;
+    min += 1;
+  }
+  return sum;
 }
 
-export {
-  sumAll,
-};
+export { sumAll };
