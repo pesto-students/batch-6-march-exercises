e7ae15f000b62c4535e481eec25c6c039e97ddb0
Mayank <opthami@gmail.com>
2019-03-28 00:24:46 +0530

    complete counter-factory

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,18 @@
 
 function applyOperator(...args) {
-  return args;
+  var operator = {
+    '+': function(a, b){return a+b},
+    '-': function(a, b){return a-b},
+    '*': function(a, b){return a*b},
+    '/': function(a, b){return a/b},
+    '%': function(a, b){return a%b}
+  };
+  if(operator.hasOwnProperty(args[0])){
+    return args.slice(1).reduce(function(total, currentValue){
+      return operator[args[0]](total, currentValue);
+    }, 0);
+  };
+  throw new Error('Please provide correct operator +, -, %, /');
 }
 
 export {
