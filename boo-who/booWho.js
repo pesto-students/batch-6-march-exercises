4106321be34a73fa60764ad995210173f0c6c8aa
Shashwat Koliwad <koliwad96@gmail.com>
2019-03-28 00:01:05 +0530

    complete day3-seg1 assignment

--- boo-who/booWho.js
+++ boo-who/booWho.js
@@ -1,6 +1,10 @@
 
-function booWho(...args) {
-  return args;
+function booWho(arg) {
+  if(typeof arg === Boolean){
+      return true; 
+  } else{
+    return false;
+  }
 }
 
 export {
--- is-odd/isOdd.js
+++ is-odd/isOdd.js
@@ -1,6 +1,13 @@
 
-function isOdd(...args) {
-  return args;
+function isOdd(num) {
+  if(num == 1 ){
+    return true;
+  }
+  else if(num %2 != 0){
+    return true;
+  } else{
+    return false;
+  }
 }
 
 export {
