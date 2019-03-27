fa0b3607dd9a03746d4489baa85122b45e011c88
Abdul Khuddus <khuddus1@gmail.com>
2019-03-28 04:45:33 +0530

    Repeat String n times - test passed

--- repeat-string-num-times/repeatStringNumTimes.js
+++ repeat-string-num-times/repeatStringNumTimes.js
@@ -1,6 +1,13 @@
 
 function repeatStringNumTimes(...args) {
-  return args;
+  var givenString = args[0]; 
+  var repetitionCount = args[1];
+  var repeatedString = "";
+  if( repetitionCount > 0 )
+  for ( var i = 0 ; i < repetitionCount ; i++ ){
+      repeatedString += givenString;
+  }
+  return repeatedString;
 }
 
 export {
