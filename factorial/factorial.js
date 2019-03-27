38adcf989c36b610e6c845b0a2fd90b12c359359
Mayank <opthami@gmail.com>
2019-03-28 00:27:56 +0530

    complete factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,6 +1,12 @@
 
-function factorial(...args) {
-  return args;
+function factorial(n) {
+  var factorial = 1;
+  
+  for(var i = 1; i <= n; i++){
+    factorial = factorial * i;
+  }
+  return factorial;
+
 }
 
 export {
