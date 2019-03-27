7898e3632ca977deeae02e6c677a04156adfb335
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-03-28 01:02:41 +0530

    pass isEven test

--- is-even/isEven.js
+++ is-even/isEven.js
@@ -1,8 +1,15 @@
-
-function isEven(...args) {
-  return args;
+function isEven(arg) {
+  let number = arg;
+  if (number === 1) {
+    return false;
+  }
+  while (number !== 1) {
+    if (number === 0) {
+      return false;
+    }
+    number /= 2;
+  }
+  return true;
 }
 
-export {
-  isEven,
-};
+export { isEven };
