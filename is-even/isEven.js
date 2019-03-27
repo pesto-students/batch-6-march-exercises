7cb1761bde8d504ab7ec4a9b734fe6e6299dda03
Abdul Khuddus <khuddus1@gmail.com>
2019-03-28 01:50:16 +0530

    Is odd completed

--- is-even/isEven.js
+++ is-even/isEven.js
@@ -1,7 +1,7 @@
 
 function isEven(...args) {
-  var givenNumber=args[0];
-  return Math.ceil(givenNumber/2)==Math.floor(givenNumber/2);
+  const givenNumber = args[0];
+  return Math.ceil( givenNumber / 2 ) == Math.floor( givenNumber / 2 ) ;
 }
 
 export {
--- is-odd/isOdd.js
+++ is-odd/isOdd.js
@@ -1,6 +1,7 @@
 
-function isOdd(...args) {
-  return args;
+function isOdd( ...args ) {
+  var givenNumber = args[0];
+  return Math.floor( givenNumber / 2 ) != Math.ceil( givenNumber / 2 );
 }
 
 export {
