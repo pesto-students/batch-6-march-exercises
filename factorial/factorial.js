a46b3a9b5c37f2a105fefef38801f240c377dab0
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-03-28 00:13:02 +0530

    pass factorial test

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,8 +1,14 @@
-
-function factorial(...args) {
-  return args;
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let result = 1;
+  let i = 1;
+  while (i <= n) {
+    result *= i;
+    i += 1;
+  }
+  return result;
 }
 
-export {
-  factorial,
-};
+export { factorial };
