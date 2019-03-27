c32a1b94b8589ed0aa23e4379af06f2cfc85d72a
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-03-28 05:09:57 +0530

    Day 3 Segment 2

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,22 @@
 
 function diffArray(...args) {
-  return args;
+  var firstArray = args[0];
+  var secondArray = args[1];
+  var differenceArray = [];
+
+  firstArray.forEach(function(value) {
+    if(secondArray.indexOf(value) === -1) {
+      differenceArray.push(value);
+    }
+  });
+
+  secondArray.forEach(function(value) {
+    if(firstArray.indexOf(value) === -1) {
+      differenceArray.push(value);
+    }
+  });
+
+  return differenceArray;
 }
 
 export {
--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,19 @@
 
 function getIndexToIns(...args) {
-  return args;
+  var array = args[0];
+  var number = args[1];
+  var index;
+  array.sort(function(a,b) {
+    return a - b;
+  });
+  
+  for(var i = 0; i < array.length; i++){
+    if(array[i] <= number ){
+      index = i;
+    }  
+  }
+  index = index === undefined ? array.length : index;
+  return index;
 }
 
 export {
