4205dcf83b479137b9f3b2957667f34e089c330a
Shashwat Koliwad <koliwad96@gmail.com>
2019-03-28 04:59:17 +0530

    complete day3-s2

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,10 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(str) {
+  var reversedString = '';
+  for(var i = str.length-1; i=>0; i--){
+    reversedString +=str[i];
+  }
+  return reversedString;
 }
 
 export {
