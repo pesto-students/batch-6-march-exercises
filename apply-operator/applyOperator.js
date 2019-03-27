acf6d74ea2d94c703268f386aaacd4ccbe18844d
Abdul Khuddus <khuddus1@gmail.com>
2019-03-28 00:12:16 +0530

    Day 3 Segment 1 : BooWho test passed

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,13 @@
 
 function applyOperator(...args) {
-  return args;
+  var operation=agrs[0];
+  if(operation==="+"){
+    var result=0;
+    for(var i=1;i<args.length;i++){
+      result=result+args[i];
+    }
+  }
+  return result;
 }
 
 export {
