779333e4cca05489188a7f4e1b9a536dd0a688f6
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-03-28 00:31:07 +0530

    complete factorial test

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,5 +1,10 @@
 
-function factorial(...args) {
+function factorial(number) {
+  if(number == 0)
+    return 0;
+  if (number == 1)
+    return 1; 
+  return number * factorial(number - 1);   
   return args;
 }
 
