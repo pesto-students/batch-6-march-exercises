7ba28346285b74647e8ee55dc1fa3ef0353e57bc
Abdul Khuddus <khuddus1@gmail.com>
2019-03-28 23:16:31 +0530

    Day 4 Segment 1 Started

--- truth-check/truthCheck.js
+++ truth-check/truthCheck.js
@@ -1,6 +1,12 @@
 
 function truthCheck(...args) {
-  return args;
+  var arrayToBeCheckedForTruth = args[0];
+  var predicateExpectedToBeTrue = args[1];
+var truthChecker = function( previousObject, currentObject) {
+    return previousObject[predicateExpectedToBeTrue]&&currentObject[predicateExpectedToBeTrue];
+};
+ return arrayToBeCheckedForTruth.reduce(truthChecker);
+  
 }
 
 export {
