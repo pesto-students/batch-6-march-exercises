dddc855187ad3c26e032a9499a0602e4890d261e
Mayank <opthami@gmail.com>
2019-03-28 18:11:36 +0530

    complete reverse-string, rot13 and sum-all

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,13 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(givenString) {
+  var givenStringCharacters = givenString.split('');
+  var charactersInReverse = []
+  for(var i = givenStringCharacters.length - 1; i >= 0; i--){
+    charactersInReverse.push(givenStringCharacters[i]);
+  }
+
+  return charactersInReverse.join('');
+  
 }
 
 export {
--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,18 @@
 
-function rot13(...args) {
-  return args;
+function rot13(encodedString) {
+  var decodedCharacters = [];
+  for(var i = 0; i < encodedString.length; i++){
+    if(/[A-Z]/.test(encodedString[i])){
+      var decodedCharacterCode= encodedString.charCodeAt(i) - 13;
+      if(decodedCharacterCode < 65){
+        decodedCharacterCode = 91 - (65 - decodedCharacterCode);
+      }
+      decodedCharacters[i] = String.fromCharCode(decodedCharacterCode);
+    }else{
+      decodedCharacters[i] = encodedString[i];
+    }
+  }
+  return decodedCharacters.join("");
 }
 
 export {
