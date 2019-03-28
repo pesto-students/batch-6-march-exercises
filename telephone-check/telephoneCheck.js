51558f57c228079287c681bada3517a39806f9eb
Abdul Khuddus <khuddus1@gmail.com>
2019-03-28 18:28:42 +0530

    binary agent completed

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,6 +1,8 @@
 
 function telephoneCheck(...args) {
-  return args;
+  var telephoneNumber = args[0];
+  var telephonePattern =/[0-9](3)[0-9](3)[0-9](4)/;
+  return telephonePattern.test(telephoneNumber);
 }
 
 export {
