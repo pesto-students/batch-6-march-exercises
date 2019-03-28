488726eff2a1a6f9f7693859d76d24e6dbb16914
Abdul Khuddus <khuddus1@gmail.com>
2019-03-28 18:06:51 +0530

    Rotated String completed

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,21 @@
+import { isArray } from "util";
 
 function steamrollArray(...args) {
-  return args;
+  var givenNestedArray = args[0];
+  var  steamRolledArray = givenNestedArray.map( function( subArray ){ 
+    return flatten( subArray );
+  });
+  
+  
+   return steamRolledArray;
+}
+function flatten(arr){
+  if (Array.isArray(arr)){
+   return flatten(arr[0]); 
+  }
+  else {
+    return arr;
+  }
 }
 
 export {
