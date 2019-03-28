c8f2d8eabbc69dad854257ebeaa897526fa12cad
Mayank <opthami@gmail.com>
2019-03-29 04:56:54 +0530

    partially complete accountDataset

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,32 @@
+const accounts = require('./dataset.json')
 
-function accountDataset(...args) {
-  return args;
+function hundredThousandairs(){
+  return accounts.bankBalances.filter((account) => {
+    return parseInt(account.amount) >= 100000;
+  })
+}
+
+function datasetWithRoundedDollar(){
+  return accounts.bankBalances.map((account)=>{
+    var updatedAccount = account;
+    updatedAccount.rounded = Math.round(parseFloat(account.amount));
+    return updatedAccount;
+  })
+}
+
+function sumOfBankBalances(){
+  var balanceAmounts = accounts.bankBalances.map((account)=>{
+    return parseFloat(account.amount);
+  })
+
+  var totalAmount = balanceAmounts.reduce((total, amount)=>{
+    return total + amount;
+  })
+  return parseFloat(totalAmount.toFixed(2));
 }
 
 export {
-  accountDataset,
+  hundredThousandairs,
+  datasetWithRoundedDollar,
+  sumOfBankBalances
 };
