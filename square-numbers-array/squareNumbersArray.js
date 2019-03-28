d667af7b65e0e8946976fca7afcced543a87f4b9
Abdul Khuddus <khuddus1@gmail.com>
2019-03-29 04:31:11 +0530

    day 4 segement 2 excercises

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,14 @@
+import { isNumber } from "util"; 
 
 function squareNumbersArray(...args) {
-  return args;
+  var numbers = args;
+  numbers.map( function ( number ) {
+    if (!isNumber(number))
+      throw('My custom error');
+    else
+      return number;
+  })
+  return numbers;
 }
 
 export {
