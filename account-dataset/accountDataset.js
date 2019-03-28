fa0cd874442bb7bb6ff396b0b7b866e72c7d9546
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-03-29 04:44:21 +0530

    [WIP] Complete day 4 segment 2
    Completed alternating-characters
    Completed savings-account
    Completed square-numbers-array

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,50 @@
+// const dataset = require('./dataset.json');
 
-function accountDataset(...args) {
-  return args;
-}
+// function hundredThousandairs() {
+//   return dataset.bankBalances.filter(bankBalance => parseInt(bankBalance.amount, 10) > 100000);
+// }
 
-export {
-  accountDataset,
-};
+// function datasetWithRoundedDollar() {
+//   return dataset.bankBalances.map(bankBalance => ({
+//     amount: bankBalance.amount,
+//     state: bankBalance.state,
+//     rounded: Math.round(bankBalance.amount),
+//   }));
+// }
+
+// function sumOfBankBalances() {
+//   const totalSum = dataset.bankBalances
+//     .reduce((existingSum, bankBalance) => existingSum + parseFloat(bankBalance.amount, 10), 0);
+//   const sumInDecimal = parseFloat(totalSum.toFixed(2));
+//   return sumInDecimal;
+// }
+
+// function sumOfInterests() {
+//   const stateBalance = dataset.bankBalances.reduce((accumulatedBalance, currentValue) => {
+//     const { state, amount } = currentValue;
+//     if (!(state in accumulatedBalance)) {
+//       accumulatedBalance[state] = amount;
+//     } else {
+//       accumulatedBalance[state] += amount;
+//     }
+//     return accumulatedBalance;
+//   });
+
+//   const getInterest = amount => amount + amount * 18.9 / 100;
+//   let totalBalance = 0;
+
+//   for (const state in stateBalance) {
+//     const balanceWithInterest = getInterest(parseFloat(stateBalance[state]));
+//     totalBalance += balanceWithInterest;
+//     console.log(totalBalance)
+//   }
+//   const balanceInDecimal = parseFloat(totalBalance.toFixed(2));
+//   return balanceInDecimal;
+// }
+
+// export {
+//   hundredThousandairs,
+//   datasetWithRoundedDollar,
+//   sumOfBankBalances,
+//   sumOfInterests,
+// };
