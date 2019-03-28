2625551bee2f8e623b0c5934e1b50de36c891cea
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-03-28 07:44:05 +0530

    pass getIndexToIns test

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,8 +1,11 @@
-
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(unsortedArray, value) {
+  const sortedArray = unsortedArray.sort(
+    (currentElement, nextElement) => currentElement - nextElement,
+  );
+  let index = sortedArray.findIndex(element => element > value);
+  index = index === -1 ? sortedArray.length : index;
+  index = sortedArray[index - 1] === value ? index - 1 : index;
+  return index;
 }
 
-export {
-  getIndexToIns,
-};
+export { getIndexToIns };
