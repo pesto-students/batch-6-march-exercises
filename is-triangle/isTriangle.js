0ae39018099930214733da55f1d6893cf947bd39
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-03-28 17:41:05 +0530

    pass isTriangle test

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,8 +1,7 @@
-
 function isTriangle(...args) {
-  return args;
+  const [fisrtSide, secondSide, thirdSide] = args;
+  const longestSide = Math.max(...args);
+  return fisrtSide + secondSide + thirdSide - longestSide > longestSide;
 }
 
-export {
-  isTriangle,
-};
+export { isTriangle };
