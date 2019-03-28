54e884332ddee217db93ea362f1edcfb2137c3e3
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-03-29 00:06:04 +0530

    Complete Day 4 Segment 1

--- object-keys/objectKeys.js
+++ object-keys/objectKeys.js
@@ -2,7 +2,7 @@
 function objectKeys(...args) {
   var obj = args[0];
   var objectIndexes = [];
-  obj.getOwnPropertyNames(obj).forEach(key => {
+  Object.getOwnPropertyNames(obj).forEach(key => {
     objectIndexes.push(key);
   });
   return objectIndexes;
