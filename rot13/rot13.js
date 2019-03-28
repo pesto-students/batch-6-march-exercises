f27dad2344ca5651f49af8e71a0632d354089e16
Abdul Khuddus <khuddus1@gmail.com>
2019-03-28 16:37:14 +0530

    Rot13 completed

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,10 @@
 
 function rot13(...args) {
-  return args;
+  var textToBeCiphered = args[0];
+  return textToBeCiphered.replace( /[A-Za-z]/g , function( singleLetter ) {
+    return String.fromCharCode( singleLetter.charCodeAt(0) + ( singleLetter.toUpperCase() <= "M" ? 13 : -13 ) );
+  } );
+   
 }
 
 export {
