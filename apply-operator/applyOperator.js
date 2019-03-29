7dab1d572b5e48fe2efa3b6a04513b3d05987cae
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-03-29 17:23:43 +0530

    pass applyOperator tests

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,8 +1,25 @@
+function applyOperator(operator, ...operands) {
+  switch (operator) {
+    case '+':
+      return operands.reduce((total, currentValue) => total + currentValue, 0);
 
-function applyOperator(...args) {
-  return args;
+    case '-':
+      return operands.reduce((difference, currentValue) => difference - currentValue, 0);
+
+    case '*':
+      return operands.reduce((product, currentValue) => product * currentValue);
+
+    case '/':
+      return parseFloat(
+        Number(operands.reduce((quotient, currentValue) => quotient / currentValue)).toFixed(4),
+      );
+
+    case '%':
+      return operands.reduce((modulus, currentValue) => modulus % currentValue);
+
+    default:
+      throw new Error('Please specify a valid operator!');
+  }
 }
 
-export {
-  applyOperator,
-};
+export default applyOperator;
