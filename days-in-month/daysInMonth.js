962f35a728e5a39b2372b1dfb98e4c8f219288b5
Mayank <opthami@gmail.com>
2019-03-30 00:08:32 +0530

    complete days in month

--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,6 +1,18 @@
 
-function daysInMonth(...args) {
-  return args;
+function daysInMonth(year, month) {
+  var monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  var isFeb = month - 1 === 1;
+  var isLeapYearFeb = year % 4 === 0 && isFeb;
+  if(isLeapYearFeb){
+    return 29;
+  }else{
+    return monthDays[month-1];
+  }
+
+
+
+
+  
 }
 
 export {
