d2f1ad31f4e8b81f6ac581b06d4b09692315b842
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-03-29 23:27:37 +0530

    pass abbreviateString tests

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,8 +1,11 @@
+import { isString } from 'util';
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(word) {
+  if (!isString(word)) {
+    throw new Error('Please enter Strings!');
+  }
+  const { 0: firstWord, [word.split(' ').length - 1]: lastWord } = word.split(' ');
+  return `${firstWord.charAt(0).toUpperCase()}${firstWord.slice(1)} ${lastWord[0].toUpperCase()}.`;
 }
 
-export {
-  abbreviateString,
-};
+export { abbreviateString };
