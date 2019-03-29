28bb648adc5b5148db954290e66f2f22aa124a9b
Abdul Khuddus <khuddus1@gmail.com>
2019-03-29 23:32:32 +0530

    Days in Month completed

--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,8 +1,21 @@
+function daysInMonth(year, month) {
+  var isLeapYear = year % 4 == 0;
 
-function daysInMonth(...args) {
-  return args;
+  var monthDayMap = {
+    1: 31,
+    2: isLeapYear ? 29 : 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+  };
+  return monthDayMap[month];
 }
 
-export {
-  daysInMonth,
-};
+export { daysInMonth };
