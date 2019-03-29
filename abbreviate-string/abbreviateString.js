1f4bfa8fd6fc67a5ee6155fd42eb55e1c31524c6
Abdul Khuddus <khuddus1@gmail.com>
2019-03-29 23:50:05 +0530

    Abbreviate String completed

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,8 +1,8 @@
-
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(myString) {
+  var wordSet = myString.split(" ");
+  var firstWord = wordSet[0];
+  var lastWord = wordSet[wordSet.length - 1];
+  return firstWord + " " + lastWord.charAt(0).toUpperCase() + ".";
 }
 
-export {
-  abbreviateString,
-};
+export { abbreviateString };
