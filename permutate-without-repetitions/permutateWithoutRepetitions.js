787ec33088f116ec929f52f08a35dd753fa1906d
Shashwat Koliwad <koliwad96@gmail.com>
2019-03-31 03:33:23 +0530

    complete  day-5-seg-2

--- permutate-without-repetitions/permutateWithoutRepetitions.js
+++ permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -1,6 +1,22 @@
+function permutateWithoutRepetitions(arr) {
+  var results = [];
 
-function permutateWithoutRepetitions(...args) {
-  return args;
+  if (arr.length === 1) {
+    results.push(arr);
+    return results;
+  }
+  const smallerPermutations = permutateWithoutRepetitions(arr.slice(1));
+
+  const firstOption = arr[0];
+    
+  for(var i = 0; i < smallerPermutations.length; i++){
+    const smallerPermutation = smallerPermutations[i];
+
+    for(var j = 0; j <= smallerPermutation.length; ++){
+      
+    }
+  }
+  return results;
 }
 
 export {
