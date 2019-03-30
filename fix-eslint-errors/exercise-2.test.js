58ccc53a5eab8595e5517beeb58278daff29fb28
Mayank <opthami@gmail.com>
2019-03-30 06:57:35 +0530

    complete exercise-1

--- fix-eslint-errors/exercise-2.test.js
+++ fix-eslint-errors/exercise-2.test.js
@@ -1,53 +1,52 @@
-function noop () {}
+function noop() {}
 
 if (typeof console === 'undefined') {
-  window.console = {
+  this.console = {
     warn: noop,
-    error: noop
-  }
+    error: noop,
+  };
 }
 
 // avoid info messages during test
-console.info = noop
 
-let asserted
 
-function createCompareFn (spy) {
-  const hasWarned = msg => {
-    var count = spy.calls.count()
-    var args
-    while (count--) {
-      args = spy.calls.argsFor(count)
+let asserted;
+
+function createCompareFn(spy) {
+  const hasWarned = (msg) => {
+    let count = spy.calls.count();
+    let args;
+    function containsMsg(arg) {
+      return arg.toString().indexOf(msg) > -1;
+    }
+    while (count) {
+      args = spy.calls.argsFor(count);
       if (args.some(containsMsg)) {
-        return true
+        return true;
       }
+      count -= 1;
     }
-
-    function containsMsg (arg) {
-      return arg.toString().indexOf(msg) > -1
-    }
-  }
+    return false;
+  };
 
   return {
-    compare: msg => {
-      asserted = asserted.concat(msg)
-      var warned = Array.isArray(msg)
-        ? msg.some(hasWarned)
-        : hasWarned(msg)
+    compare: (msg) => {
+      asserted = asserted.concat(msg);
+      const warned = Array.isArray(msg) ? msg.some(hasWarned) : hasWarned(msg);
       return {
         pass: warned,
         message: warned
-          ? 'Expected message "' + msg + '" not to have been warned'
-          : 'Expected message "' + msg + '" to have been warned'
-      }
-    }
-  }
+          ? `Expected message "' ${msg}  '" not to have been warned`
+          : `Expected message "' ${msg}  '" to have been warned`,
+      };
+    },
+  };
 }
 
 // define custom matcher for warnings
 beforeEach(() => {
-  asserted = []
-  spyOn(console, 'warn')
+  asserted = [];
+  spyOn(console, 'warn');
   spyOn(console, 'error')
   jasmine.addMatchers({
     toHaveBeenWarned: () => createCompareFn(console.error),
