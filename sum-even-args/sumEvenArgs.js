51d23cd9d3bb545eed34957c1ddf5bea435c8876
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-02 02:40:49 +0530

    WIP day 6 segment 1

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,11 @@
 
-function sumEvenArgs(...args) {
-  return args;
+function sumEvenArgs(...array) {
+  return array.reduce((totalOfEvenNumbers, currentNum) => {
+    if (currentNum % 2 === 0) {
+      return totalOfEvenNumbers + currentNum;
+    }
+    return totalOfEvenNumbers;
+  }, 0);
 }
 
 export {
--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,6 +1,11 @@
 
-function tripleAndFilter(...args) {
-  return args;
+function tripleAndFilter(array) {
+  return array.reduce((accumulator, currentItem) => {
+    if ((currentItem * 3) % 5 === 0) {
+      return accumulator.concat(currentItem * 3);
+    }
+    return accumulator;
+  }, []);
 }
 
 export {
