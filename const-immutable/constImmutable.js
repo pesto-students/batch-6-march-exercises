0ba93c6a84445741be8a3cac68c1052982b23089
Mayank <opthami@gmail.com>
2019-04-02 02:14:19 +0530

    complete const immutable

--- const-immutable/constImmutable.js
+++ const-immutable/constImmutable.js
@@ -1,8 +1,14 @@
 
-function constImmutable(...args) {
-  return args;
+function constImmutable() {
+  const account = {
+    username: 'pesto',
+  };
+  Object.defineProperty(account, 'password', {
+    value: 'initialPassword',
+    writable: false,
+  });
+  return account.password;
 }
-
 export {
   constImmutable,
 };
