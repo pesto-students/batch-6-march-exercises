7900e2c18760d32695c52e50d4eed0a99206f24a
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-02 05:02:06 +0530

    WIP day 6 segment 2

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,37 @@
+class Queue {
+  constructor() {
+    this.linkedList = [];
+  }
 
-function queue(...args) {
-  return args;
+  enqueue(parameter) {
+    this.linkedList = this.linkedList.concat(parameter);
+  }
+
+  dequeue() {
+    if (this.linkedList.length === 0) {
+      return null;
+    }
+    const firstElement = this.linkedList[0];
+    this.linkedList = this.linkedList.slice(1);
+    return firstElement;
+  }
+
+  toString(stringifier) {
+    if (this.linkedList[0] instanceof Object) {
+      return this.linkedList.map(stringifier).join(',');
+    }
+    return this.linkedList.join(',');
+  }
+
+  peek() {
+    return this.linkedList.length === 0 ? null : this.linkedList[0];
+  }
+
+  isEmpty() {
+    return this.linkedList.length === 0;
+  }
 }
 
 export {
-  queue,
+  Queue,
 };
