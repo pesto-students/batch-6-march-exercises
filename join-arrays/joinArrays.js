d67141fd3c9c0e0bb3e584acdbe572c8c24664b1
Abdul Khuddus <khuddus1@gmail.com>
2019-04-02 02:05:23 +0530

    Day 6 Segment 1 started

--- join-arrays/joinArrays.js
+++ join-arrays/joinArrays.js
@@ -1,6 +1,6 @@
 
-function joinArrays(...args) {
-  return args;
+function joinArrays(...arrays) {
+  return arrays.reduce((concatinatedArray, array) => concatinatedArray.concat(array));
 }
 
 export {
