351c8dd3b0766df42ff43b5fd0938d5e56db5a1c
Abdul Khuddus <khuddus1@gmail.com>
2019-04-02 02:39:03 +0530

    double Odd Numbers completed

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,12 @@
 
-function doubleOddNumbers(...args) {
-  return args;
+function doubleOddNumbers(oddNumbers) {
+  return oddNumbers.reduce((oddNumbersDoubledArray, number) => {
+    const isNumberOdd = number % 2 === 1;
+    if (isNumberOdd) {
+      oddNumbersDoubledArray.push(number + number);
+    }
+    return oddNumbersDoubledArray;
+  }, []);
 }
 
 export {
