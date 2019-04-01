ab62fc5f24419afef42d3aa9bf949deed9b37c46
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-02 02:39:31 +0530

    Complete Day 6 Segment 1

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,6 +1,19 @@
 
-function createStudent(...args) {
-  return args;
+function createStudent({ likesJavaScript = true, likesES2015 = true } = {}) {
+  const studentLikesNothing = likesJavaScript === false && likesES2015 === false;
+  if (studentLikesNothing) {
+    return 'The student does not like much...';
+  }
+
+  const studentLikesBothTopic = likesJavaScript === true && likesES2015 === true;
+  if (studentLikesBothTopic) {
+    return 'The student likes JavaScript and ES2015';
+  }
+
+  if (likesJavaScript) {
+    return 'The student likes JavaScript!';
+  }
+  return 'The student likes ES2015!';
 }
 
 export {
