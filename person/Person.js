5248a634248888fb30e3fbca39ff12b76f232308
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-02 04:52:44 +0530

    Complete Day 6 Segment 2

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,19 @@
 
-function person(...args) {
-  return args;
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  addDobDigits() {
+    const dobChars = this.dateOfBirth.split('');
+    const isNumber = el => (Number.isInteger(parseInt(el, 10)));
+    const dobDigits = dobChars.filter(isNumber);
+    return dobDigits.reduce((sum, el) => sum + parseInt(el, 10), 0);
+  }
 }
 
 export {
-  person,
+  Person,
 };
