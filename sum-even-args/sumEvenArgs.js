b2ec737dbee0250a069dd56c4a0119452cd1cfdd
Shashwat Koliwad <koliwad96@gmail.com>
2019-04-02 02:37:25 +0530

    complete day_5_seg_1

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,7 +1,7 @@
 
-function sumEvenArgs(...args) {
-  return args;
-}
+const sumEvenArgs= ((arr) =>{
+  return arr.filter((i) => i%2 === 0).reduce((sum, ele) => sum + ele);
+})
 
 export {
   sumEvenArgs,
