fe88a8b1fca810007aaeefb93a4400f4a6949e79
Mayank <opthami@gmail.com>
2019-04-03 02:17:27 +0530

    complete fibonacciIter

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,20 @@
 
-function fibonacciIter(...args) {
-  return args;
-}
+const fibonacciIter = {
+  previousN: 1,
+  currentN: 1,
+  [Symbol.iterator]() {
+    return this;
+  },
+  next() {
+    const newNumber = this.previousN + this.currentN;
+    this.previousN = this.currentN;
+    this.currentN = newNumber;
+    return {
+      value: newNumber,
+      done: false,
+    };
+  },
+};
 
 export {
   fibonacciIter,
