32a3978dabf82f8415cfce2921f796e5a7657cd3
Abdul Khuddus <khuddus1@gmail.com>
2019-04-03 02:10:44 +0530

    Is Iterable empty completed

--- is-iterable-empty/isIterableEmpty.js
+++ is-iterable-empty/isIterableEmpty.js
@@ -1,6 +1,11 @@
 
-function isIterableEmpty(...args) {
-  return args;
+function isIterableEmpty(objects) {
+  let objectCount = 0;
+  // eslint-disable-next-line no-unused-vars
+  for (const obj of objects) {
+    objectCount += 1;
+  }
+  return objectCount === 0;
 }
 
 export {
