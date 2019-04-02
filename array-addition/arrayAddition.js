ef1c338e0f6685da816325cbb2feadfd85b11dac
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-03 04:25:09 +0530

    WIP day 7 segment 2

--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,6 +1,24 @@
 
-function arrayAddition(...args) {
-  return args;
+function arrayAddition(firstArray, secondArray) {
+  if (firstArray.length === 0 || secondArray.length === 0
+    || !Array.isArray(firstArray) || !(Array.isArray(secondArray))) {
+    throw Error();
+  }
+
+  const [longerArray, shorterArray] = firstArray.length > secondArray.length
+    ? [firstArray, secondArray]
+    : [secondArray, firstArray];
+
+  const addedArray = longerArray.map((number, index) => {
+    if (!Number.isInteger(number)) {
+      throw Error();
+    }
+    if (index <= shorterArray.length - 1) {
+      return number + shorterArray[index];
+    }
+    return number;
+  });
+  return addedArray;
 }
 
 export {
