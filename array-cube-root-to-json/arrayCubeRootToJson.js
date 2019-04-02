a7ad3434815c1c63fca2afe863bcdf1d0887880c
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-03 04:49:19 +0530

    WIP day 7 segment 2

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,7 +1,27 @@
+/* eslint-disable no-param-reassign */
+function arrayCubeRootToJson(numbers) {
+  if (!Array.isArray(numbers)) {
+    throw Error();
+  }
+  numbers = numbers.map((number) => {
+    if (typeof number === 'boolean') {
+      throw Error();
+    }
+    if (number === Infinity) {
+      return number;
+    }
+    return parseInt(number, 10);
+  });
 
-function arrayCubeRootToJson(...args) {
-  return args;
+  if (numbers.some(Number.isNaN)) {
+    throw Error();
+  }
+  return numbers.reduce((json, currentNumber) => {
+    json[currentNumber] = Math.cbrt(currentNumber);
+    return json;
+  }, {});
 }
+
 
 export {
   arrayCubeRootToJson,
