1e9ab8d04f330766f2a6b5e63780309330578f7d
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-03 04:28:26 +0530

    Complete Day 7 Segment 1

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,6 +1,17 @@
 
-function arrayCubeRootToJson(...args) {
-  return args;
+function arrayCubeRootToJson(arr) {
+  const { isArray } = Array;
+  if (!isArray(arr)) {
+    throw new Error('Not an Array');
+  }
+  const allNumbers = arr.every(Number);
+  if (!allNumbers) {
+    throw new Error('Not valid array');
+  }
+  return arr.reduce((res, el) => {
+    res[el.toString()] = Math.cbrt(el);
+    return res;
+  }, {});
 }
 
 export {
--- object-assign/objectAssign.js
+++ object-assign/objectAssign.js
@@ -1,6 +1,6 @@
 
-function objectAssign(...args) {
-  return args;
+function objectAssign(...objects) {
+  return objects.reduce((res, obj) => Object.assign(res, obj));
 }
 
 export {
