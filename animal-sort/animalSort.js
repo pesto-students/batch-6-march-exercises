e23c37e7bb9fee355df3b9ac3cced56192d953b6
Shashwat Koliwad <koliwad96@gmail.com>
2019-04-03 04:36:17 +0530

    complete day-7-seg-2

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,10 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(arr) {
+  if(arr.length){
+    var sortable = []
+    return sortable = arr.sort((a1,a2) => (a1.numberOfLegs > a2.numberOfLegs) ? 1: -1)
+  }
+  return [];
 }
 
 export {
