530f6101bea44efd044a01d24a54c0ba3b7d5363
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-03 23:16:14 +0530

    Negative index

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,18 @@
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(array) {
+  const getPositiveIndex = (arr, index) => (index < 0 ? arr.length + parseInt(index, 10) : index);
+
+  const handler = {
+    get: (targetArray, key) => {
+      const positiveIndex = getPositiveIndex(targetArray, key);
+      return targetArray[positiveIndex];
+    },
+    set: (targetArray, key, value) => {
+      const positiveIndex = getPositiveIndex(key);
+      return Reflect.set(targetArray, positiveIndex, value);
+    },
+  };
+  return new Proxy(array, handler);
 }
 
 export {
