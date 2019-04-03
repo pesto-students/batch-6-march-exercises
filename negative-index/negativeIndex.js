b11a4aa121e6e154701b9f70445a85f119185828
Mayank <opthami@gmail.com>
2019-04-04 02:52:46 +0530

    WIP day 8 segment 1

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,7 +1,13 @@
-
 function negativeIndex(array) {
   const handler = {
-    get: (target, property) => target[target.length + property],
+    get(target, key) {
+      return target[key];
+    },
+    set(target, key, value) {
+      const newArr = target;
+      newArr[key] = value;
+      return newArr;
+    },
   };
   const proxy = new Proxy(array, handler);
   return proxy;
