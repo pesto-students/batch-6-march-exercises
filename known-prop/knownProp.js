b2f3b304884841fd455f9feb2a8416a32bb0dcd1
Shashwat Koliwad <koliwad96@gmail.com>
2019-04-03 22:49:50 +0530

    complete day8-seg1

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,7 +1,12 @@
 
-function knownProp(...args) {
-  return args;
+var knownProp = new Proxy(receiver, {
+  get: function(obj, prop, receiver) {
+      return prop in obj ?
+          obj[prop] :
+          37;
+  }
 }
+
 
 export {
   knownProp,
