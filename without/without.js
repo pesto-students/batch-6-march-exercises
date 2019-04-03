9952668a56e9823a55cf322ceb28e6fbd58c718c
Mayank <opthami@gmail.com>
2019-04-03 23:07:34 +0530

    wip without

--- without/without.js
+++ without/without.js
@@ -1,6 +1,8 @@
 
-function without(...args) {
-  return args;
+function without(array1, array2) {
+  const isInArray1 = item => array1.indexOf(item) === -1;
+
+  return array2.filter(isInArray1);
 }
 
 export {
