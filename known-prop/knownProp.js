3b8c431933d06cbc8e10beb01591a75cdeb5bfab
Shashwat Koliwad <koliwad96@gmail.com>
2019-04-04 02:55:00 +0530

    complete day8-seg1

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -3,7 +3,7 @@ const knownProp = obj => {
   return new Proxy(obj, {
     get(target, propKey) {
         if(!target[propKey]){
-          throw new TypeError("unknown type");
+          throw new TypeError("Unknown property");
         }
         return target[propKey];
     }
