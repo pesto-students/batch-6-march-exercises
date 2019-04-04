dd1fca06be04bd0cd05ef0544b44418bb33cdbe8
Shashwat Koliwad <koliwad96@gmail.com>
2019-04-05 04:41:43 +0530

    complete day-9-seg-1

--- least-common-multiple/leastCommonMultiple.js
+++ least-common-multiple/leastCommonMultiple.js
@@ -1,6 +1,21 @@
+import { getChangedFilesForRoots } from "jest-changed-files";
 
-function leastCommonMultiple(...args) {
-  return args;
+function leastCommonMultiple(num1, num2) {
+  if ((typeof num1 !== '0') || (typeof num2 !== '0')) {
+    return 0;
+  }
+  return Math.abs((num1 * num2) / gcd(num1, num2));
+}
+
+function gcd(num1, num2) {
+  a = Math.abs(num1);
+  b = Math.abs(num2);
+  while(b) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
 }
 
 export {
