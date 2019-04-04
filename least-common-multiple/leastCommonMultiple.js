5ff2edbf90a8ec7f7eb588052211e9857df72fd8
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-05 04:29:52 +0530

    Day 9 Segment 2

--- least-common-multiple/leastCommonMultiple.js
+++ least-common-multiple/leastCommonMultiple.js
@@ -1,6 +1,19 @@
 
 function leastCommonMultiple(...args) {
-  return args;
+  const number1 = Math.abs(args[0]);
+  const number2 = Math.abs(args[1]);
+  if (number1 === 0 || number2 === 0) {
+    return 0;
+  }
+
+  const smallerNumber = number1 < number2 ? number1 : number2;
+  const biggerNumber = number1 > number2 ? number1 : number2;
+
+  let lcm = biggerNumber;
+  while (lcm % smallerNumber !== 0) {
+    lcm += biggerNumber;
+  }
+  return lcm;
 }
 
 export {
