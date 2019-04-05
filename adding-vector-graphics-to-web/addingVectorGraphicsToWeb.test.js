c05400df8d43f99e2bbafbc808dd268bda35f5b6
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-04-05 07:08:25 +0530

    [WIP] complete day 9 segment 2

---
+++ adding-vector-graphics-to-web/addingVectorGraphicsToWeb.test.js
@@ -0,0 +1,5 @@
+describe('vector graphics', () => {
+  it('Passes test', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ canvas/canvas.test.js
@@ -0,0 +1,5 @@
+describe('canvas', () => {
+  it('Passes test', () => {
+    expect(true).toEqual(true);
+  });
+});
