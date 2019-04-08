d32e236702cc26555a1c1a9eceab1488f082c78b
Mayank <opthami@gmail.com>
2019-04-08 21:38:27 +0530

    as

--- cycled/Cycled.js
+++ cycled/Cycled.js
@@ -1,8 +1,39 @@
+class Cycled {
+  constructor(array) {
+    this.array = array;
+    this.index = 0;
+  }
 
-function cycled(...args) {
-  return args;
+  next() {
+    return this[Symbol.iterator]().next().value;
+  }
+
+  [Symbol.iterator]() {
+    const { array } = this;
+    let { index } = this;
+    return {
+      next() {
+        const currentItem = array[index];
+        index += 1;
+        if (index >= array.length) {
+          index = 0;
+        }
+        this.index = index;
+        return { value: currentItem, done: false };
+      },
+    };
+  }
+
+  current() {
+    return this.array[this.index];
+  }
+
+  previous() {
+    const previousIndex = this.index === 0 ? this.array.length - 1 : this.index - 1;
+    return this.array[previousIndex];
+  }
 }
 
 export {
-  cycled,
+  Cycled,
 };
