ae9a0039a650db4637afca3273ff7ca85148bed0
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-08 08:27:50 +0530

    Day 6 Segment 1

--- block-scoping/blockScoping.js
+++ block-scoping/blockScoping.js
@@ -2,7 +2,10 @@
 function blockScoping(index) {
   const callbacks = [];
   for (let i = 0; i <= 10; i += 1) {
-    callbacks.push(() => i);
+    callbacks.push(() => {
+      const a = i;
+      return a;
+    });
   }
   return callbacks[index]();
 }
