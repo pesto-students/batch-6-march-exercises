803c35d8e7d987a4e17c40b296bc835e4d8ac570
Mayank <opthami@gmail.com>
2019-04-09 16:39:10 +0530

    complete question 11

--- question-11/index.js
+++ question-11/index.js
@@ -1,6 +1,8 @@
-const page = document.getElementById('title');
-const childNode = page.childNodes[0];
+const title = document.getElementById('title');
+const content = document.getElementById('content');
+const childNode = title.childNodes[0];
 const nodeValues = [childNode.nodeValue, 'newValue1', 'newValue2'];
+
 let index = 0;
 // eslint-disable-next-line no-unused-vars
 const changeValue = () => {
@@ -9,4 +11,13 @@ const changeValue = () => {
     index = 0;
   }
   childNode.nodeValue = nodeValues[index];
+};
+let bigFont = false;
+
+// eslint-disable-next-line no-unused-vars
+const changeAttribute = () => {
+  bigFont = !bigFont;
+  const attributeNames = content.getAttributeNames();
+  const newAttrValue = bigFont ? 'font-size: 2rem' : 'font-size: 1rem';
+  content.setAttribute(attributeNames[1], newAttrValue);
 };
---
+++ question-11/question11.test.js
@@ -0,0 +1,5 @@
+describe('submit question 11', () => {
+  it('should submit question 11', () => {
+    expect(true).toBe(true);
+  });
+});
