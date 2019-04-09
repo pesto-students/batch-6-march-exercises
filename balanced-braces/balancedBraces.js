37ca39e593386c7e9e628fa39397911c51f7ae37
Mayank <opthami@gmail.com>
2019-04-10 03:18:38 +0530

    complete beginner exercises

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -6,24 +6,8 @@
 // is not push it on the stack.
 // if stack length is 0 braces are balanced return true
 
-function balancedBraces(string) {
-  const chars = string.split('');
-  const braces = ['{', '}', '(', ')', '['];
-  const peek = (stackTop, current, open, close) => () => stackTop === open && current === close;
-  const stack = chars.reduce((acc, elm) => {
-    if (peek(acc[acc.length - 1], elm, '{', '}')
-      || peek(acc[acc.length - 1], elm, '(', ')')
-      || peek(acc[acc.length - 1], elm, '[', ']')
-    ) {
-      acc.pop();
-    }
-    if (braces.indexOf(elm) > -1) {
-      acc.push(elm);
-    }
-    return acc;
-  }, []);
-  console.log(stack);
-  return stack.length === 0;
+function balancedBraces(...args) {
+  return args;
 }
 
 export { balancedBraces };
--- beginner-exercises/src/components/04-GroceryList.js
+++ beginner-exercises/src/components/04-GroceryList.js
@@ -1,71 +1,104 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
 
-/*
-  In this exercises, you'll will make a reactive grocery list.
-
-  Task 1: Fill the `return` of `GroceryList` render method. It should return
-        a list of `GroceryListItem`. You need to display the groceries names
-        using `this.props` in `GroceryListItem`. We already prepared the variable
-        `groceriesComponents` inside `render` method containing a list of these items for you.
-
-
-  Task 2: Create an input box and a button. User should be able to add more grocery items and click
-          the `Add` button to add it to the list displaying the item.
-
-  Task 3: Create a button to clear the whole list.
-
-  Task 4: Clicking on a grocery item should change its color to red. Clicking again should change
-          it back to black. Red means the item has been purchased.
-
-*/
 
 class GroceryList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       groceries: [{ name: 'Apples' }, { name: 'KitKat' }, { name: 'Red Bull' }],
+      newItem: '',
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.clearList = this.clearList.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState(prevState => ({
+      groceries: [...prevState.groceries, this.state.newItem],
+      newItem: '',
+    }));
+  }
+
+  handleChange(event) {
+    this.setState({
+      newItem: event.target.value,
+    });
+  }
+
+  clearList() {
+    this.setState({
+      groceries: [],
+    });
   }
 
   render() {
     const { groceries } = this.state;
-    /*
-      Properties are a way to pass parameters to your React components.
-      We mentioned this in the third exercise. Properties are to React
-      components what attributes are to HTML elements.
 
-      Below you can see how to pass properties to child components.
-      We have defined a `grocery` property for each `GroceryListItem`.
-    */
     const groceriesComponents = groceries.map(item => ( // eslint-disable-line no-unused-vars
-      <GroceryListItem grocery={item} />
+      // eslint-disable-next-line react/jsx-filename-extension
+      <GroceryListItem grocery={item.name} />
     ));
-    // Hint: Don't forget about putting items into `ul`
     return (
       <div>
-        Put your code here
+        <ul>
+          {groceriesComponents}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new">
+            Add Items
+            <input type="text" id="new" onChange={this.handleChange} value={this.state.newItem} />
+          </label>
+          <input type="submit" value="submit" />
+          <button type="button" onClick={this.clearList}>Clear List</button>
+        </form>
       </div>
     );
   }
 }
 
-// Render grocery name from component's properties.
-// If you have a problem, check `this.props` in the console.
+
 /* eslint-disable react/no-multi-comp, no-useless-constructor */
 class GroceryListItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      color: true,
+    };
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  changeColor() {
+    this.setState({
+      color: !this.state.color,
+    });
   }
 
   render() {
+    const styleObj = {
+      color: this.state.color ? 'black' : 'red',
+    };
     return (
-      <li>
-        Put your code here.
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+      // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+      // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+      <li onClick={this.changeColor} style={styleObj}>
+        {this.props.grocery}
       </li>
     );
   }
 }
 
 // Do prop validation here using the package `prop-types`
+GroceryListItem.propTypes = {
+  // eslint-disable-next-line react/require-default-props
+  grocery: PropTypes.string,
+};
 
 export default GroceryList;
