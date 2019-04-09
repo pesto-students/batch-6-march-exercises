806dcac7aa92d3ac0f51b1bb2c04f7e28f2b354f
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-04-09 11:35:34 +0530

    Completed rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,21 @@
+function rot13(string) {
+  const charCodes = [];
+  for (let i = 0; i < string.length; i += 1) {
+    const charCode = string.charCodeAt(i);
+    if (charCode >= 65 && charCode < 78) { // handle A-M
+      charCodes.push(charCode + 13);
+    } else if (charCode >= 78 && charCode <= 90) { // handle N-Z
+      charCodes.push(charCode - 13);
+    } else if (charCode >= 97 && charCode < 110) { // handle a-m
+      charCodes.push(charCode + 13);
+    } else if (charCode >= 110 && charCode <= 122) { // handle n-z
+      charCodes.push(charCode - 13);
+    } else {
+      charCodes.push(charCode);
+    }
+  }
 
-function rot13(...args) {
-  return args;
+  return String.fromCharCode(...charCodes);
 }
 
 export {
