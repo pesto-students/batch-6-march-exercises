10694be3e65c8ac0678fa63904f0953e7118b2e0
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-04-09 10:04:06 +0530

    Completed is-pallendrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,8 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(value) {
+  const string = String(value);
+  const matchedString = string.toLowerCase().match(/[a-z0-9]/ig);
+  return matchedString.join() === matchedString.reverse().join();
 }
 
 export {
