493350a699323d97dd8e01c1e91e69591f89b852
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-10 22:55:46 +0530

    Complte parseMs

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,32 +1,17 @@
 
-const SECONDS = 1000;
-const MINUTES = SECONDS * 60;
-const HOURS = MINUTES * 60;
-const DAYS = HOURS * 24;
-
 function parseMs(milliseconds) {
-  var obj = {
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-    milliseconds: 0
-  }
-  obj.days = parseInt(milliseconds / DAYS);
-  milliseconds = milliseconds - obj.days * DAYS;
+  const seconds = milliseconds / 1000;
+  const minutes = seconds / 60;
+  const hours = minutes / 60;
+  const days = hours / 24;
 
-  obj.hours = parseInt(milliseconds / HOURS);
-  milliseconds = milliseconds - obj.hours * HOURS;
-
-  obj.minutes = parseInt(milliseconds / MINUTES);
-  milliseconds = milliseconds - obj.minutes * MINUTES;
-
-  obj.seconds = parseInt(milliseconds / SECONDS);
-  milliseconds = milliseconds - obj.seconds * SECONDS;
-
-  obj.milliseconds = milliseconds;
-
-  return obj;
+  return {
+    days: Math.trunc(days),
+    hours: Math.trunc(hours % 24),
+    minutes: Math.trunc(minutes % 60),
+    seconds: Math.trunc(seconds % 60),
+    milliseconds: Math.trunc(milliseconds % 1000),
+  };
 }
 
 export {
