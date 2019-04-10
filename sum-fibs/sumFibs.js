757ff2fcadd394b246f9fd38819133ecdcc84292
Mayank <opthami@gmail.com>
2019-04-10 09:58:26 +0530

    complete sumFibs

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,15 @@
 
-function sumFibs(...args) {
-  return args;
+function sumFibs(number, prev = 0, current = 1, sum = 0) {
+  const prevVal = current;
+  const currentVal = prev + current;
+  if (currentVal > number) {
+    return sum + 1;
+  }
+  if (currentVal % 2 !== 0) {
+    const newSum = sum + currentVal;
+    return sumFibs(number, prevVal, currentVal, newSum);
+  }
+  return sumFibs(number, prevVal, currentVal, sum);
 }
 
 export {
