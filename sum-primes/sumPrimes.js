30716f3c53f3db605521ef84be83e0c83a71bb8f
Mayank <opthami@gmail.com>
2019-04-10 10:25:18 +0530

    complete sumPrimes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,7 +1,21 @@
 
-function sumPrimes(...args) {
-  return args;
+function sumPrimes(number) {
+  let sum = 0;
+  for (let i = 1; i <= number; i += 1) {
+    let isPrime = true;
+    for (let j = 1; j <= Math.sqrt(i); j += 1) {
+      if (i % j === 0 && j !== 1) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime && i > 1) {
+      sum += i;
+    }
+  }
+  return sum;
 }
+
 
 export {
   sumPrimes,
