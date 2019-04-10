385f178d4e5265908bd417f0f6ac902c5f1cf6ed
Mayank <opthami@gmail.com>
2019-04-10 08:36:57 +0530

    complete is triangle

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,15 @@
 
-function isTriangle(...args) {
-  return args;
+function isTriangle(side1, side2, side3) {
+  if (side1 + side2 < side3) {
+    return false;
+  }
+  if (side1 + side3 < side2) {
+    return false;
+  }
+  if (side2 + side3 < side1) {
+    return false;
+  }
+  return true;
 }
 
 export {
