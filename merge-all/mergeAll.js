ae7bb83e4e1df5123002107f15daee050e3fe6ba
Mayank <opthami@gmail.com>
2019-04-12 01:12:06 +0530

    complete merge all

--- merge-all/mergeAll.js
+++ merge-all/mergeAll.js
@@ -1,6 +1,7 @@
 
-function mergeAll(...args) {
-  return args;
+function mergeAll(array) {
+  const objectsArray = array.filter(item => typeof item === 'object');
+  return objectsArray.reduce((acc, elm) => Object.assign(acc, elm), {});
 }
 
 export {
