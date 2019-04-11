46ee3bfb1720e00088aeb70f432fad9774dd2d54
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-12 00:01:19 +0530

    Commplete promiseAllProps

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,14 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(promiseObject) {
+  const keys = Object.keys(promiseObject);
+  const allPromises = [];
+  for (const key of keys) {
+    allPromises.push(promiseObject[key]);
+  }
+  return Promise.all(allPromises)
+    .then(results => results.reduce((acc, result, currentIndex) => (
+      { ...acc, [keys[currentIndex]]: result }
+    ), {}));
 }
 
 export {
