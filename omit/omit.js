7c8ed92652a73082d385280ea3a4cbdd7512aec8
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-12 04:05:32 +0530

    Complete Omit

--- omit/omit.js
+++ omit/omit.js
@@ -1,7 +1,14 @@
+/* eslint-disable guard-for-in */
 
-function omit(...args) {
-  return args;
+function omit(keysToOmit, obj) {
+  const keys = [];
+  for (const key in obj) {
+    keys.push(key);
+  }
+  const filteredKeys = keys.filter(key => !keysToOmit.includes(key));
+  return filteredKeys.reduce((acc, key) => ({ ...acc, [key]: obj[key] }), {});
 }
+
 
 export {
   omit,
