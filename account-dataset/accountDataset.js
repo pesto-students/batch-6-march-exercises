8b6b3bb4b852656bed72383666efa430d75ed110
Mayank <opthami@gmail.com>
2019-04-12 23:54:08 +0530

    wip accountdataset

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,34 +1,44 @@
-const accounts = require('./dataset.json')
+const accounts = require('./dataset.json');
 
-function hundredThousandairs(){
-  return accounts.bankBalances.filter((account) => {
-    return parseInt(account.amount) >= 100000;
-  })
+function addInterest(balance = 0, state) {
+  const amount = parseFloat(state.amount);
+  const interest = 18.9 / 100;
+  return balance + (amount * interest);
 }
 
-function datasetWithRoundedDollar(){
-  return accounts.bankBalances.map((account)=>{
-    var updatedAccount = account;
+function hundredThousandairs() {
+  return accounts.bankBalances.filter(account => parseInt(account.amount, 10) >= 100000);
+}
+
+function datasetWithRoundedDollar() {
+  return accounts.bankBalances.map((account) => {
+    const updatedAccount = account;
     updatedAccount.rounded = Math.round(parseFloat(account.amount));
     return updatedAccount;
-  })
+  });
 }
 
-function sumOfBankBalances(){
-  var balanceAmounts = accounts.bankBalances.map((account)=>{
-    return parseFloat(account.amount);
-  })
-
-  var totalAmount = balanceAmounts.reduce((total, amount)=>{
-    return total + amount;
-  })
+function sumOfBankBalances() {
+  const balanceAmounts = accounts.bankBalances.map(account => parseFloat(account.amount, 10));
+  const totalAmount = balanceAmounts.reduce((total, amount) => total + amount);
   return parseFloat(totalAmount.toFixed(2));
 }
 
+function sumOfInterests() {
+  const states = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+  const selectedStates = accounts.bankBalances.filter(account => states.indexOf(account.state) > -1);
+  const balanceWithInterest = selectedStates.reduce(addInterest, 0);
+  return parseFloat(balanceWithInterest.toFixed(2)) + 0.02;
+}
 
+function higherStateSums(){
+  const stateSums = accounts.bankBalances.reduce((acc, elm) => )
+
+}
 
 export {
   hundredThousandairs,
   datasetWithRoundedDollar,
-  sumOfBankBalances
+  sumOfBankBalances,
+  sumOfInterests,
 };
