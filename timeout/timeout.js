3ecd25dff549dc5600ce06a96e86e0761641235a
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-12 19:18:11 +0530

    Day 6 Segment 2

--- timeout/timeout.js
+++ timeout/timeout.js
@@ -1,6 +1,8 @@
 
-function timeout(...args) {
-  return args;
+function timeout(string) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(string), 500);
+  }).then(value => `Hey ${value}`);
 }
 
 export {
