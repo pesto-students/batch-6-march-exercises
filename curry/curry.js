1b33205a93abf8a361aeac665a637f854cdfdfc9
Mayank <opthami@gmail.com>
2019-04-14 02:24:20 +0530

    complete curry

--- curry/curry.js
+++ curry/curry.js
@@ -1,6 +1,13 @@
+// curry(fn) => fn(...args) => fn(...args)
 
-function curry(...args) {
-  return args;
+// add(1)(2)
+function curry(cb, ...initialArguments) {
+  if (cb.length === initialArguments.length) {
+    return cb(...initialArguments);
+  }
+  return function curryIng(...args) {
+    return curry(cb, ...initialArguments, ...args);
+  };
 }
 
 export {
