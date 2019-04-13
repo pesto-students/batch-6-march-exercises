35a82b49bf43095ea8385406dd9dc3e9b86a1d74
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-14 04:53:50 +0530

    WIP Day 5 segment 1

--- throw-errors/throwErrors.js
+++ throw-errors/throwErrors.js
@@ -1,8 +1,11 @@
-
-function throwErrors(...args) {
-  return args;
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-mutable-exports */
+let errorName;
+try {
+  const a = b;
+} catch (e) {
+  errorName = e.name;
 }
 
-export {
-  throwErrors,
-};
+export { errorName };
--- time-was/timeWas.js
+++ time-was/timeWas.js
@@ -1,8 +1,41 @@
+function timeWas(timestamp, now = Date.now()) {
+  const SECONDS = 1000;
+  const MINUTES = SECONDS * 60;
+  const HOURS = MINUTES * 60;
+  const DAYS = HOURS * 24;
+  const WEEKS = DAYS * 7;
+  const MONTHS = DAYS * 30;
+  const YEARS = MONTHS * 12;
 
-function timeWas(...args) {
-  return args;
+  const timeDiff = now - timestamp;
+  if (timeDiff < 5000) {
+    return 'just now';
+  }
+  if (timeDiff >= YEARS) {
+    return `${parseInt(timeDiff / YEARS, 10)} years ago`;
+  }
+  if (timeDiff >= MONTHS) {
+    return `${parseInt(timeDiff / MONTHS, 10)} months ago`;
+  }
+  if (timeDiff >= WEEKS) {
+    return `${parseInt(timeDiff / WEEKS, 10)} weeks ago`;
+  }
+  if (timeDiff >= DAYS) {
+    return `${parseInt(timeDiff / DAYS, 10)} days ago`;
+  }
+  if (timeDiff >= HOURS) {
+    return `${parseInt(timeDiff / HOURS, 10)} hours ago`;
+  }
+  if (timeDiff >= MINUTES) {
+    if (parseInt(timeDiff / MINUTES, 10) === 1) {
+      return `${parseInt(timeDiff / MINUTES, 10)} minute ago`;
+    }
+    return `${parseInt(timeDiff / MINUTES, 10)} minutes ago`;
+  }
+  if (timeDiff >= SECONDS) {
+    return `${parseInt(timeDiff / SECONDS, 10)} seconds ago`;
+  }
+  return '';
 }
 
-export {
-  timeWas,
-};
+export { timeWas };
