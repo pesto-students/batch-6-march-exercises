1e88a7525c2917d1b72f10e339ae2ea4168288f9
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-14 03:11:17 +0530

    WIP Day 4 Segment 2

--- curry/curry.js
+++ curry/curry.js
@@ -1,8 +1,9 @@
-
-function curry(...args) {
-  return args;
+function curry(fn, ...args) {
+  if (fn.length === args.length) {
+    // all needed arguments received?
+    return fn(...args);
+  }
+  // return a fn that takes the remaining args and returns curried fn
+  return (...rest) => curry(fn, ...args, ...rest);
 }
-
-export {
-  curry,
-};
+export { curry };
