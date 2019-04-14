d27e9437909402cde7e6ad207b6e0bc2dfa41b66
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-15 03:21:11 +0530

    Complete Day 4 Segment 1

--- update-inventory/updateInventory.js
+++ update-inventory/updateInventory.js
@@ -1,8 +1,22 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable prefer-destructuring */
+function updateInventory(currentInventory, newInventory) {
+  const toObject = nestedArray => nestedArray.reduce((acc, currentArray) => {
+    acc[currentArray[1]] = currentArray[0];
+    // console.log(acc);
+    return acc;
+  }, {});
 
-function updateInventory(...args) {
-  return args;
+  const currentInventoryObject = toObject(currentInventory);
+  newInventory.map((arr) => {
+    if (Object.prototype.hasOwnProperty.call(currentInventoryObject, arr[1])) {
+      currentInventoryObject[arr[1]] += arr[0];
+    } else {
+      currentInventoryObject[arr[1]] = arr[0];
+    }
+  });
+
+  return Object.keys(currentInventoryObject).map(key => [currentInventoryObject[key], key]);
 }
 
-export {
-  updateInventory,
-};
+export { updateInventory };
