76af27fa35c72f11ec248e398b74c7c5536ca4b1
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-04-14 06:06:22 +0530

    Complete arithmeic-expression-evaluator

--- arithmetic-expression-evaluator/arithmeticExpressionEvaluator.js
+++ arithmetic-expression-evaluator/arithmeticExpressionEvaluator.js
@@ -1,6 +1,41 @@
 
-function arithmeticExpressionEvaluator(...args) {
-  return args;
+const calculate = (a, b, op) => {
+  switch (op) {
+    case '*': return a * b;
+    case '/': return a / b;
+    case '+': return a + b;
+    case '-': return a - b;
+    default: throw new Error('Unknown operation');
+  }
+};
+
+function arithmeticExpressionEvaluator(expr) {
+  if (typeof expr !== 'string') {
+    throw new TypeError('Expected string expression to evaluate');
+  }
+  const invalidChars = /[^\d+\-*/ \t\n]/;
+  if (invalidChars.test(expr)) {
+    throw new Error('Invalid character passed in the expression');
+  }
+
+  const exprMatcher = /[\d+\-*/]{1,}/g;
+  const exprArray = expr.match(exprMatcher);
+  const operators = ['*', '/', '-', '+'];
+  for (let i = 0; i < operators.length; i += 1) {
+    const operator = operators[i];
+    while (true) { // eslint-disable-line no-constant-condition
+      const operatorIndex = exprArray.indexOf(operator);
+      if (operatorIndex === -1) {
+        break;
+      }
+      const firstOperand = Number(exprArray[operatorIndex - 1]);
+      const secondOperand = Number(exprArray[operatorIndex + 1]);
+      const result = calculate(firstOperand, secondOperand, operator);
+      exprArray.splice(operatorIndex - 1, 3, result);
+    }
+  }
+  const result = exprArray[0];
+  return result;
 }
 
 export {
