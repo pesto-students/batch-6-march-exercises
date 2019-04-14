502592a612d57c4fcadcd7043a5fd164005d0e78
Mayank <opthami@gmail.com>
2019-04-15 03:01:22 +0530

    complete day 6 segment 2

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,8 +1,21 @@
 
 
+const shiftcurrent = (char) => {
+  let code = char.charCodeAt(0) + 1;
+
+  if (/[a-z]/g.test(char)) {
+    code = code > 122 ? (code % 122) + 96 : code;
+  }
+  if (/[A-Z]/g.test(char)) {
+    code = code > 122 ? (code % 122) + 96 : code;
+  }
+  return code;
+};
+
+
 function alphabeticShift(text) {
   const chars = text.split('');
-  const shiftChar = character => String.fromCharCode((character.charCodeAt(0) + 1));
+  const shiftChar = character => String.fromCharCode(shiftcurrent(character));
   return chars.reduce((acc, char) => `${acc}${shiftChar(char)}`, '');
 }
 
