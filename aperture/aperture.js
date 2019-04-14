6f4622b3fce04db5bb3bf5a26c62d5d73f425937
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-15 03:52:36 +0530

    Complete Day 7 Segment 2

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,13 @@
 
-function aperture(...args) {
-  return args;
+function aperture(tuplesLength, arr) {
+  let tuples = [];
+  let index = 0;
+  while (index < arr.length - tuplesLength + 1) {
+    const tempArr = arr.slice(index, tuplesLength + index);
+    tuples = [...tuples, tempArr];
+    index += 1;
+  }
+  return tuples;
 }
 
 export {
--- arithmetic-expression-evaluator/arithmeticExpressionEvaluator.js
+++ arithmetic-expression-evaluator/arithmeticExpressionEvaluator.js
@@ -1,6 +1,46 @@
+const calculate = (operator, firstNumber, secondNumber) => {
+  switch (operator) {
+    case '/':
+      return firstNumber / secondNumber;
+    case '*':
+      return firstNumber * secondNumber;
+    case '+':
+      return firstNumber + secondNumber;
+    case '-':
+      return firstNumber - secondNumber;
+    default:
+      return undefined;
+  }
+};
 
-function arithmeticExpressionEvaluator(...args) {
-  return args;
+const evaluate = (str) => {
+  let numbers = str.match(/(\w+)/g);
+  numbers = numbers.map(Number);
+  const operators = str.match(/([+\-*\/])/g);
+  const operatorsToEvaluate = ['/', '*', '-', '+'];
+
+  for (const operator of operatorsToEvaluate) {
+    while (operators.includes(operator)) {
+      const index = operators.indexOf(operator);
+
+      numbers[index] = calculate(operator, numbers[index], numbers[index + 1]);
+      operators.splice(index, 1);
+      numbers.splice(index + 1, 1);
+    }
+  }
+
+  return numbers[0];
+};
+
+const invalidExpression = str => !(/^[+\-*\/0-9]+$/.test(str));
+function arithmeticExpressionEvaluator(expressionString) {
+  const expression = expressionString.replace(/\s/g, '');
+
+  if (invalidExpression(expression)) {
+    throw new Error('Invalid Expression');
+  }
+
+  return evaluate(expression);
 }
 
 export {
