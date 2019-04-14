87a891d5f827c7487dbe743d0c66ea96056839f6
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-14 12:07:52 +0530

    WIP Day 7  segment 1

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,8 +1,16 @@
-
-function fibonacciIter(...args) {
-  return args;
-}
-
-export {
-  fibonacciIter,
+const fibonacciIter = {
+  previousFib: 0,
+  currentFib: 1,
+  [Symbol.iterator]() {
+    this.previousFib = 0;
+    this.currentFib = 1;
+    return this;
+  },
+  next() {
+    const obj = { value: this.previousFib + this.currentFib, done: false };
+    [this.previousFib, this.currentFib] = [this.currentFib, this.previousFib + this.currentFib];
+    return obj;
+  },
 };
+
+export { fibonacciIter };
