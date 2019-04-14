2cd57285f647c1c20032879ef2a6b98b3d27f36b
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-04-14 06:18:43 +0530

    Complete aparture

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,14 @@
 
-function aperture(...args) {
-  return args;
+function aperture(tupleLength, array) {
+  if (tupleLength > array.length) {
+    return [];
+  }
+  const result = [];
+  const numOfTuple = array.length + 1 - tupleLength;
+  for (let i = 0; i < numOfTuple; i += 1) {
+    result.push(array.slice(i, i + tupleLength));
+  }
+  return result;
 }
 
 export {
