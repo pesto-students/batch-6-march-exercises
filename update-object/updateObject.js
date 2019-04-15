d3d438b3d6a0d4aa6752cb68f7f2fae86a192ed4
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-04-16 00:57:36 +0530

    update-object

--- update-object/updateObject.js
+++ update-object/updateObject.js
@@ -1,6 +1,12 @@
 
-function updateObject(...args) {
-  return args;
+function updateObject(idx, item, array) {
+  const index = idx < 0 ? array.length + idx : idx;
+  if (index < 0 || index >= array.length) {
+    return array;
+  }
+  const newArray = [...array];
+  newArray[index] = item;
+  return newArray;
 }
 
 export {
