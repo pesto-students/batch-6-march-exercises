cd3bdc6ee38d1bf24f054c90184023bf87e444e7
Mayank <opthami@gmail.com>
2019-04-15 16:10:42 +0530

    complete rangeIter

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,6 +1,33 @@
+function rangeIter(startVal, endVal) {
+  const isNotNumber = val => typeof val === 'number';
+  if (!isNotNumber(startVal) || !isNotNumber(endVal)) {
+    throw TypeError('undefined is not a number');
+  }
 
-function rangeIter(...args) {
-  return args;
+
+  if (startVal > endVal) {
+    return [];
+  }
+  if (startVal === endVal) {
+    return [startVal];
+  }
+
+  let initalVal = startVal;
+
+  return {
+    [Symbol.iterator]() {
+      initalVal = startVal;
+      return this;
+    },
+    next() {
+      if (initalVal <= endVal) {
+        const obj = { value: initalVal, done: false };
+        initalVal += 1;
+        return obj;
+      }
+      return { done: true };
+    },
+  };
 }
 
 export {
