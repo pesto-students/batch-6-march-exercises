9e5f259bcaaa9236dec93b21414bc6d3b922ac12
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-15 12:54:37 +0530

    Day 8 Segment 2

--- uncurry-n/uncurryN.js
+++ uncurry-n/uncurryN.js
@@ -1,14 +1,31 @@
 
 function uncurryN(number, curryFn) {
-  if (number === 1) {
-    return args => curryFn(args);
-  }
-
-  return (...arg) => {
-    const c = curryFn(arg[0]);
-    return uncurryN(number - 1, c);
+  const innerFunc = (...args) => {
+    const curryFnLength = curryFn.length;
+    if (args.length <= curryFnLength || number === 1) {
+      return curryFn(...args);
+    }
+    const curryFnArgs = args.splice(0, curryFnLength);
+    return uncurryN(number - 1, curryFn(...curryFnArgs))(...args);
   };
+
+  Object.defineProperty(innerFunc, 'length', {
+    value: number,
+    writable: false,
+    enumerable: false,
+    configurable: true,
+  });
+
+  return innerFunc;
 }
+
+
+// function a3(a) {
+//   return b => c => a + b + c;
+// }
+
+// const uncurried = uncurryN(3, a3);
+// console.log(uncurried(1, 2, 3));
 
 export {
   uncurryN,
