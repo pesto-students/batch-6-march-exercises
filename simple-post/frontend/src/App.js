cc56b59393235eec0daa8c0f551b5f78d6921d6a
Mayank <opthami@gmail.com>
2019-04-16 04:37:58 +0530

    simple-post: implement localstorage for current page

--- simple-post/frontend/src/App.js
+++ simple-post/frontend/src/App.js
@@ -1,74 +1,76 @@
 import React, { Component, Fragment } from 'react';
 
 import Post from './components/Post';
+import { getPageData } from './apis/posts.api';
 import './App.css';
 
-const baseUrl = 'http://localhost:3001/posts/';
+// eslint-disable-next-line no-undef
+const { localStorage } = window;
 
+const updateLocalStorage = (field, cb) => {
+  const currentValue = localStorage.getItem(field);
+  const newValue = cb(parseInt(currentValue, 10));
+  localStorage.setItem(field, newValue);
+  return newValue;
+};
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: [],
-      currentPage: 1,
+      // eslint-disable-next-line eqeqeq
+      disablePrev: localStorage.getItem('currentPage') == 1,
     };
-
-    this.getPageData = this.getPageData.bind(this);
     this.handleNextClick = this.handleNextClick.bind(this);
     this.handlePrevClick = this.handlePrevClick.bind(this);
   }
 
   componentDidMount() {
-    this.getPageData();
+    let currentPage = localStorage.getItem('currentPage');
+    if (!currentPage) {
+      currentPage = updateLocalStorage('currentPage', x => x);
+    }
+    getPageData.call(this, currentPage);
   }
 
-  getPageData() {
-    return fetch(`${baseUrl}${this.state.currentPage}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'pesto-password': 'darth vader',
-      },
-    })
-      .then(response => response.json())
-      .then(response => this.setState({
-        posts: response.data,
-      }));
-  }
-
+  // eslint-disable-next-line class-methods-use-this
   handleNextClick() {
+    const currentPage = updateLocalStorage('currentPage', item => item + 1);
+    getPageData.call(this, currentPage);
     this.setState({
-      currentPage: this.state.currentPage += 1,
+      disablePrev: false,
     });
-    this.getPageData();
   }
 
+  // eslint-disable-next-line class-methods-use-this
   handlePrevClick() {
-    this.setState({
-      currentPage: this.state.currentPage -= 1,
-    });
-    this.getPageData();
+    const currentPage = updateLocalStorage('currentPage', item => item - 1);
+    if (currentPage < 0) {
+      this.setState({
+        disablePrev: true,
+      });
+    }
+    getPageData.call(this, currentPage);
   }
 
   render() {
     return (
+      // eslint-disable-next-line react/jsx-filename-extension
       <div>
         <h2>Posts</h2>
         <hr />
         <br />
-        <button onClick={this.handlePrevClick} disabled={this.state.currentPage === 1}>
+        <button onClick={this.handlePrevClick} disabled={this.state.disablePrev}>
           Previous
         </button>
-        <button onClick={this.handleNextClick}>
-          Next
-        </button>
+        <button onClick={this.handleNextClick}>Next</button>
         <div>
           {this.state.posts.map(post => (
             <Fragment key={post.id}>
               <Post post={post} />
               <hr />
             </Fragment>
-            ))}
+          ))}
         </div>
       </div>
     );
