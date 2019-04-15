6189eccdd0cfa42479748394f58c080af4e9dc4d
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-15 10:05:30 +0530

    pass balanced braces test

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,8 +1,25 @@
-
-function balancedBraces(...args) {
-  return args;
+function balancedBraces(string) {
+  const regex = /[{|}|[|\]|(|)]/g;
+  const match = string.match(regex);
+  const stack = [];
+  const oppBraces = {
+    '}': '{',
+    ']': '[',
+    ')': '(',
+  };
+  for (const brace of match) {
+    if (brace === '{' || brace === '[' || brace === '(') {
+      stack.push(brace);
+    }
+    if (brace === '}' || brace === ']' || brace === ')') {
+      if (stack[stack.length - 1] === oppBraces[brace]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
 
-export {
-  balancedBraces,
-};
+export { balancedBraces };
