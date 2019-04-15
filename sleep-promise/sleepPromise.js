4e852cabeaefa264f18ad012755c041b6c7363d9
Mayank <opthami@gmail.com>
2019-04-15 20:13:33 +0530

    partially complete sleep promise

--- sleep-promise/sleepPromise.js
+++ sleep-promise/sleepPromise.js
@@ -1,18 +1,6 @@
 
-function sleepPromise(delay) {
-  return new Promise(resolve => setTimeout(resolve, delay));
-}
-
 function sleep(delay) {
-  const newSleepPromise = sleepPromise(delay);
-
-  function functionThen(value) {
-    return newSleepPromise.then(() => value);
-  }
-
-  functionThen.then = args => newSleepPromise.then(args);
-  functionThen.catch = Promise.resolve().catch;
-  return functionThen;
+  return new Promise(resolve => setTimeout(resolve, delay));
 }
 
 export {
