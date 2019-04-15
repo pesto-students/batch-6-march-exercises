af663825de79686c062147f90d4067b87be0e1f6
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-15 10:00:38 +0530

    Complete balanced braces

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,6 +1,25 @@
 
-function balancedBraces(...args) {
-  return args;
+const openingBraces = ['{', '(', '['];
+const closingBraces = ['}', ')', ']'];
+const correspondingOpeningBrace = {
+  '}': '{',
+  ')': '(',
+  ']': '[',
+};
+function balancedBraces(string) {
+  const stack = [];
+
+  for (const char of string) {
+    if (openingBraces.includes(char)) {
+      stack.push(char);
+    } else if (closingBraces.includes(char)) {
+      const lastBrace = stack.pop();
+      if (lastBrace !== correspondingOpeningBrace[char]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
 
 export {
