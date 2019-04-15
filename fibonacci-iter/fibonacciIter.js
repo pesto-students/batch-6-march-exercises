47a41e90b0e6516420560bf08c2ff9e11e4a271b
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-15 09:07:55 +0530

    check generator fn.

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -13,4 +13,15 @@ const fibonacciIter = {
   },
 };
 
+// function* fiboGenerator(n, current = 0, next = 1) {
+//   if (n === 0) {
+//     return 0;
+//   }
+//   yield current;
+//   yield* fiboGenerator(n - 1, next, next + current);
+// }
+
+// const [...fib10] = fiboGenerator(10);
+// console.log(fib10);
+
 export { fibonacciIter };
