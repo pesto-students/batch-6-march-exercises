0f9d46c90103a341c695728fdacd0a9a21da2013
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-17 00:43:44 +0530

    Complete filteredNames

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,6 +1,12 @@
 
-function filteredNames(...args) {
-  return args;
+const fs = require('fs');
+
+const hasExtn = extn => fileName => fileName.endsWith(`.${extn}`);
+
+function filteredNames(directoryPath, fileExtn) {
+  const extnValidator = hasExtn(fileExtn);
+  const fileNames = fs.readdirSync(directoryPath);
+  return fileNames.filter(extnValidator);
 }
 
 export {
