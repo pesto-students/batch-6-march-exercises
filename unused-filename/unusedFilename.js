9b27367cc791a62de0e730b03b2d0b4855ec196c
Mayank <opthami@gmail.com>
2019-04-17 02:00:48 +0530

    WIP day 17 segment 1

--- unused-filename/unusedFilename.js
+++ unused-filename/unusedFilename.js
@@ -1,8 +1,34 @@
+const fs = require('fs');
 
-function unusedFilename(...args) {
-  return args;
+const fsPromises = fs.promises;
+const path = require('path');
+
+const directory = path.join(__dirname, '/fixtures');
+
+const checkForFile = (files, filePath, sync = false) => {
+  const [dir, fileName] = filePath.split('/');
+
+  if (files.indexOf(fileName) > -1) {
+    const sortedFiles = files.sort();
+    const fileIndex = sortedFiles.length;
+    const fileNameParts = fileName.split('.');
+    fileNameParts[0] = `${fileNameParts[0]}(${fileIndex})`;
+    const newFile = `${dir}/${fileNameParts.join('')}`;
+    if (sync) {
+      return fs.writeFileSync(newFile, 'hello', { encoding: 'utf8' });
+    }
+    return fsPromises.writeFile(newFile).then(() => fileName);
+  }
+  return filePath;
+};
+
+function unusedFilename(filePath) {
+  return fsPromises.readdir(directory).then(files => checkForFile(files, filePath));
 }
 
-export {
-  unusedFilename,
+unusedFilename.sync = (filePath) => {
+  const files = fs.readdirSync(directory);
+  return checkForFile(files, filePath, true);
 };
+
+export { unusedFilename };
