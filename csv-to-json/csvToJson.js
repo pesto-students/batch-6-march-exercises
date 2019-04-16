a42eb2835299713dc64e79498601e2b4e710d164
Mayank <opthami@gmail.com>
2019-04-17 05:10:47 +0530

    complete csvToJson

--- csv-to-json/csvToJson.js
+++ csv-to-json/csvToJson.js
@@ -1,8 +1,29 @@
+const fs = require('fs');
 
-function csvToJson(...args) {
-  return args;
+function csvToJson(csvFile) {
+  const stream = fs.createReadStream(csvFile, { encoding: 'utf8' });
+  let csvData;
+  let jsonData;
+  return new Promise((resolve) => {
+    stream.on('data', (data) => {
+      csvData = data.split('\n');
+      const rows = csvData.map(row => row.split(','));
+      const shData = rows.slice(1);
+      jsonData = shData.reduce((acc, elm) => {
+        const [name, superheroname, power] = elm;
+        acc.push({
+          name,
+          superheroname,
+          power,
+        });
+        return acc;
+      }, []);
+    });
+    stream.on('close', () => {
+      resolve(jsonData);
+    });
+  });
 }
 
-export {
-  csvToJson,
-};
+
+export { csvToJson };
