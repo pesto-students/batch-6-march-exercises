eca3209002a6b53e5f4ec7280e24ec05e1d72eab
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-04-18 01:04:53 +0530

    WIP Day 18 Segment 1

--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+// const ejs = require('ejs');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', (req, res) => res.redirect('/form-with-get'));
 
-app.get('/', (req, res) => {
-  return res.redirect('/form-with-get');
+app.get('/form-with-get', (req, res) => res.render('form-with-get'));
+app.get('/submit-form-with-get', (req, res) => {
+  res.json(req.query);
 });
 
-app.get('/form-with-get', (req, res) => {
-  return res.render('form-with-get');
-});
-
-app.get('/form-with-post', (req, res) => {
-  return res.render('form-with-post');
+app.get('/form-with-post', (req, res) => res.render('form-with-post'));
+app.post('/submit-form-with-post', (req, res) => {
+  res.json(req.body);
 });
 
 app.listen(3000, () => {
