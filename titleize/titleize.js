105ffd3f3033d505776f0aa715924114b36ff0bd
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-04-20 00:39:27 +0530

    [WIP] Complete day 20 segment 1

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,6 +1,7 @@
 
-function titleize(...args) {
-  return args;
+function titleize(string) {
+  const matcher = /([a-z])([a-z]*)/ig;
+  return string.replace(matcher, (match, p1, p2) => `${p1.toUpperCase()}${p2.toLowerCase()}`);
 }
 
 export {
--- unique-random/uniqueRandom.js
+++ unique-random/uniqueRandom.js
@@ -1,6 +1,18 @@
 
-function uniqueRandom(...args) {
-  return args;
+function uniqueRandom(low, high) {
+  let prev = null;
+  const diff = high - low;
+
+  const randGenerator = () => Math.floor(Math.random() * diff) + low;
+
+  return () => {
+    let res = randGenerator();
+    while (res === prev) {
+      res = randGenerator();
+    }
+    prev = res;
+    return res;
+  };
 }
 
 export {
