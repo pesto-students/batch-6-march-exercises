7669f7a41f491b347f51fe8e6ab611660c0162f0
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-20 01:02:40 +0530

    Complete Day 20 Segment 1

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,6 +1,21 @@
 
-function titleize(...args) {
-  return args;
+const isAlphabet = (char) => {
+  const charCode = char.charCodeAt(0);
+  return (charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122);
+};
+
+function titleize(string) {
+  let lastCharAlphabet = false;
+  let newString = '';
+  for (const char of string) {
+    let currentChar = char.toLowerCase();
+    if (lastCharAlphabet === false) {
+      currentChar = char.toUpperCase();
+    }
+    lastCharAlphabet = isAlphabet(currentChar);
+    newString += currentChar;
+  }
+  return newString;
 }
 
 export {
