3129b1286ccea9f3fa8b4abe63d1cbc41d8e1493
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-22 10:35:16 +0530

    Complete Day 20 Segment 2

--- modify-object-keys/modifyObjectKeys.js
+++ modify-object-keys/modifyObjectKeys.js
@@ -1,6 +1,13 @@
 
-function modifyObjectKeys(...args) {
-  return args;
+function modifyObjectKeys(object, callback) {
+  const keys = Object.keys(object);
+  const newObj = {};
+  for (const key of keys) {
+    const newKey = callback(key);
+    const descriptors = Object.getOwnPropertyDescriptor(object, key);
+    Object.defineProperty(newObj, newKey, descriptors);
+  }
+  return newObj;
 }
 
 export {
